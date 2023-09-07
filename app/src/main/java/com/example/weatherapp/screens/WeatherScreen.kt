36e491c1e890319fa79b913dc80d54a2e3a67b1a
caglarkaya <ramis.caglar.kaya@gelirler.gov.tr>
2023-09-07 15:40:29 +0300

    Deneme

---
+++ app/src/main/java/com/example/weatherapp/screens/WeatherScreen.kt
@@ -0,0 +1,13 @@
+package com.example.weatherapp.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun WeatherScreen(
+    navController: NavController,
+    locationKey: String,
+    locationName: String,
+    country: String,
+) {
+}
